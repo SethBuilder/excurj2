bad32b1540c9515bb5fda1d52e9305a454e77420
Seif <seif@seif.rocks>
2017-05-09 13:55:54 +0300

    populated gender, refactored code

--- population_script.py
+++ population_script.py
@@ -34,71 +34,62 @@ def populate_cities():
 	GoogleKey = 'AIzaSyDViGwJgWL18QSKvPozvAiqloyy1pW2lxg'
 	# GoogleKey = 'AIzaSyB1E9CZaaaw1c77A7eZSophK_LnaGX5XRQ'
 
-	for i in range(len(city_names)):
-		query = city_names[i] + "+" + countries[i] # to be sent to the Google Places API
-		
-		url = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
-			'?query=%s'
-			'&key=%s') % (query, GoogleKey)
+	if City.objects.all().count() == 0 or len(glob.glob('media/city_images/*.jpg')) == 0:
+		for i in range(len(city_names)):
+			query = city_names[i] + "+" + countries[i] # to be sent to the Google Places API
+			
+			url = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
+				'?query=%s'
+				'&key=%s') % (query, GoogleKey)
 
-		#grabbing the JSON results
-		with urllib.request.urlopen(url) as response:
-			jsonraw = response.read()
-			jsondata = json.loads(jsonraw)
+			#grabbing the JSON results
+			with urllib.request.urlopen(url) as response:
+				jsonraw = response.read()
+				jsondata = json.loads(jsonraw)
 
-		#now we'll use json results to extract city ID and city image
-		city_id = jsondata['results'][0]['id']
+			#now we'll use json results to extract city ID and city image
+			city_id = jsondata['results'][0]['id']
 
-		#create a City object
-		created_city = City.objects.get_or_create(city_id=city_id)[0]
+			#create a City object
+			created_city = City.objects.get_or_create(city_id=city_id)[0]
 
-		#save name, country, description
-		created_city.name = city_names[i]
-		created_city.country = countries[i]
-		# send city and country name to wikipedia and exctract first 5 sentences
-		created_city.description = wikipedia.summary(city_names[i] + ' ' + countries[i]) 
+			#save name, country, description
+			created_city.name = city_names[i]
+			created_city.country = countries[i]
+			# send city and country name to wikipedia and exctract first 5 sentences
+			created_city.description = wikipedia.summary(city_names[i] + ' ' + countries[i]) 
 
-		#extract city's 'photo reference' that we'll send to google places photo API
-		city_image_ref = jsondata['results'][0]['photos'][0]['photo_reference']
+			#extract city's 'photo reference' that we'll send to google places photo API
+			city_image_ref = jsondata['results'][0]['photos'][0]['photo_reference']
 
-		#set max width / can be changed if front end requires it
-		maxwidth = '400'
+			#set max width / can be changed if front end requires it
+			maxwidth = '400'
 
-		#The URL the HTTP Response to which brings the image
-		city_image_url = ('https://maps.googleapis.com/maps/api/place/photo'
-		'?maxwidth=%s'
-		'&photoreference=%s'
-		'&key=%s') % (maxwidth, city_image_ref, GoogleKey)
+			#The URL the HTTP Response to which brings the image
+			city_image_url = ('https://maps.googleapis.com/maps/api/place/photo'
+			'?maxwidth=%s'
+			'&photoreference=%s'
+			'&key=%s') % (maxwidth, city_image_ref, GoogleKey)
 
-		#check if the image exists already
-		if not os.path.isfile("media/city_pictures/"+city_names[i] + '.jpg'):
-			#only get the remote image if the file is not there
-			try:
-				#get the image
-				retrieved_image = requests.get(city_image_url)
-				sleep(5)#sleep to avoid too many requests in short period of time
-			except requests.exceptions.ConnectionError as e:
-				e.status_code = 'Connection refused from the Google API'
+			#check if the image exists already
+			if not os.path.isfile("media/city_pictures/"+city_names[i] + '.jpg'):
+				#only get the remote image if the file is not there
 
-			#create local file to save remote image
-			with open(city_names[i] + '.jpg', 'wb') as f:
-				#write the remote image to the local file we just created
-				f.write(retrieved_image.content)
-
-				#open the local file with the image inside it
-				with open(city_names[i] + '.jpg', 'rb') as reopen:
-					#convert it to a Django File
-					django_file = File(reopen)
-					#save it to the ImageField -> args are: 1. the name of the file that is to be saved in MEDIA_ROOT path 
-					#2. The Django file itself 3. save instance
-					if not created_city.city_image or not os.path.isfile("media/city_pictures/"+city_names[i] + '.jpg'):#if ImageField is empty then save image
-						created_city.city_image.save(city_names[i] + '.jpg', django_file, save=True)
+				django_file = save_image(city_image_url, city_names[i] + '.jpg')
+				print("image pulled")
+				
+				if not created_city.city_image or not os.path.isfile("media/city_pictures/"+city_names[i] + '.jpg'):#if ImageField is empty then save image
+					created_city.city_image.save(city_names[i] + '.jpg', django_file, save=True)
+					django_file.close()
 				
 
 
-		created_city.save()# save City object
-		
-		cities.append(created_city)
+			created_city.save()# save City object
+			
+			cities.append(created_city)
+
+		else:
+			cities = City.objects.all()
 
 		
 	#delete local files as they're already uploaded to media root
@@ -113,16 +104,16 @@ def populate_users():
 	""" populates User and UserProfile objects """
 
 	#have a list of City objects at hand
-	if City.objects.all().count() == 0 or len(glob.glob('media/city_images/*.jpg')) == 0:
-		cities = populate_cities()
+	if City.objects.all().count() == 0 or len(glob.glob('media/city_images/*.jpg')) == 0:#if there's no City objects or no pics
+		cities = populate_cities()#call the function
 	else:
-		cities = City.objects.all()
+		cities = City.objects.all()#else, call them from the db
 
 	# random data URLs
-	urls = ['https://randomuser.me/api/?nat=gb&results=10', 'https://randomuser.me/api/?nat=fr&results=10', 
-	'https://randomuser.me/api/?nat=de&results=10', 'https://randomuser.me/api/?nat=us&results=10', 
-	'https://randomuser.me/api/?results=10', 'https://randomuser.me/api/?results=10', 'https://randomuser.me/api/?results=10&nat=es', 
-	'https://randomuser.me/api/?results=10', 'https://randomuser.me/api/?results=10', 'https://randomuser.me/api/?results=10&nat=ca']
+	urls = ['https://randomuser.me/api/?nat=gb&results=1', 'https://randomuser.me/api/?nat=fr&results=2', 
+	'https://randomuser.me/api/?nat=de&results=1', 'https://randomuser.me/api/?nat=us&results=2', 
+	'https://randomuser.me/api/?results=0', 'https://randomuser.me/api/?results=1', 'https://randomuser.me/api/?results=2&nat=es', 
+	'https://randomuser.me/api/?results=1', 'https://randomuser.me/api/?results=0', 'https://randomuser.me/api/?results=1&nat=ca']
 
 	#go through random data urls
 	for i in range(len(urls)):
@@ -151,6 +142,7 @@ def get_users(users):
 	return user_list
 
 def get_profiles(user_list, city, users_in_json):
+	profiles = []
 	for i in range(len(user_list)):
 
 		#now create User Profile
@@ -162,48 +154,48 @@ def get_profiles(user_list, city, users_in_json):
 		print(users_in_json['results'][i]['dob'])
 		print(users_in_json['results'][i]['picture']['large'])# TO DELETE
 
-		#retrieve the profile pic
-		try:
-			retrieved_image = requests.get(users_in_json['results'][i]['picture']['large'])
-			sleep(5)
+		django_file = save_image(users_in_json['results'][i]['picture']['large'], str(profile.user.id) + '.jpg')
 
-		except requests.exceptions.ConnectionError as e:
-			e.status_code = 'Connection refused from random user API'
+		profile.prof_pic.save(str(profile.user.id) + '.jpg', django_file, save=True)
+		django_file.close()
 
-		#create local file to save remote image
-		with open(str(profile.user.id) + '.jpg', 'wb') as f:
-			#write the remote image to the local file we just created
-			f.write(retrieved_image.content)
-
-		#open the local file with the image inside it
-		with open(str(profile.user.id) + '.jpg', 'rb') as reopen:
-			#convert it to a Django File
-			django_file = File(reopen)
-			profile.prof_pic.save(str(profile.user.id) + '.jpg', django_file, save=True)
+		profile.sex = users_in_json['results'][i]['gender']
 
 		profile.save()
+		profiles.append(profile)
+
+	#delete local files as they're already uploaded to media root
+	for profile in profiles:
+		if os.path.isfile(str(profile.user.id) + '.jpg'):
+			os.remove(str(profile.user.id) + '.jpg')
+			print("removed local file: "+ str(profile.user.id))
+
 
 def save_image(url, file_name):
-	"""retrieves an image from a URL and returns a Django file"""
+	""" pulls an image from a URL and returns a Django File """
 	#retrieve the profile pic
 	try:
 		retrieved_image = requests.get(url)
-		sleep(5)
+		sleep(2)
 
 	except requests.exceptions.ConnectionError as e:
-		e.status_code = 'Connection refused from random user API'
+		e.status_code = 'Connection refused from random API'
 
 	#create local file to save remote image
-	with open(str(file_name) + '.jpg', 'wb') as f:
+	with open(file_name, 'wb') as f:
 		#write the remote image to the local file we just created
 		f.write(retrieved_image.content)
 
 	#open the local file with the image inside it
-	with open(str(file_name) + '.jpg', 'rb') as reopen:
-		#convert it to a Django File
-		django_file = File(reopen)
-	return django_file
+	# with open(file_name + '.jpg', 'rb') as reopen:
+	# 	#convert it to a Django File
+	# 	django_file = File(reopen)
 
+	reopen = open(file_name, 'rb')
+	django_file = File(reopen)
+
+	print("django is: " + str(django_file.closed))
+	return django_file
 
 if __name__ == '__main__':
 	print('starting populate.py')
