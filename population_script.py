f93cd0f49c2f4a8f4449d58eaf98cb139f6da345
Seif <seif@seif.rocks>
2017-05-09 23:03:48 +0300

    populated excursions, offers and requests

--- population_script.py
+++ population_script.py
@@ -3,15 +3,16 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excurj_proj.settings')
 import django
 django.setup()
 import urllib.request, json
-from excurj.models import City
+from excurj.models import City, UserProfile, Excursion, Offer, Request
 from django.contrib.auth.models import User
-from excurj.models import UserProfile
 from django.core.files import File
 import requests
 import wikipedia
 from time import sleep
 import datetime
 import glob
+import random
+
 
 
 def get_json(url):
@@ -201,8 +202,120 @@ def save_image(url, file_name):
 
 	return django_file
 
+def populate_excursions():
+
+	""" travelers list excursions they wish to have by listing a city (destination) and a message.
+		For example: "Hey great people of London!, coming your way, would love to meet up with some of you!!"
+	 """
+	excursions = []
+
+	#prepare a list of all User objects
+	user_list = User.objects.all()
+
+	#prepare a list of all City objects
+	city_list = City.objects.all()
+
+	#prepare a list of possible messages
+	messages = []
+
+	#populate messages travelers put on their excursions
+	for city in city_list:
+		msg1 = "Hey awesome people of " + city.name + "!" + " I'm arriving in your city quit soon and would love to meet some of you!"
+		msg2 = "I'm visiting " + city.name + " with a friend quit soon! we're nice and friendly and would love to meet up!"
+		msg3 = "Hello, Hello, I'm solo traveling around the world and soon to arrive in " + city.name + "!" + " would love to meet you some locals, maybe to walk around town and have lunch or dinner, Ciao, Ciao and talk to you soon!! :)"
+
+		messages.extend((msg3,msg2, msg1))
+
+	# #generate a random date from today till last day of the year
+	# start_date = datetime.date.today().toordinal()
+	# end_date = datetime.date.today().replace(day=31, month=12).toordinal()
+
+	#generate 10 excursions
+	for i in range(10):
+		#generate random date
+	 	# date = datetime.date.fromordinal(random.randint(start_date, end_date))
+
+	 	#create Excursion object
+	 	excursion = Excursion(traveler=random.choice(user_list), 
+	 		city=random.choice(city_list), message=random.choice(messages), date=generate_date() )
+
+	 	excursion.save()
+	 	excursions.append(excursion)
+
+	return excursions
+
+def generate_date():
+	""" generate a date between today and the end of the year """
+	#generate a random date from today till last day of the year
+	start_date = datetime.date.today().toordinal()
+	end_date = datetime.date.today().replace(day=31, month=12).toordinal()
+	date = datetime.date.fromordinal(random.randint(start_date, end_date))
+
+	return date
+
+
+def populate_offers():
+	""" populates offers made by locals to travelers coming to their city """
+	#first, prepare a list of all users
+	user_list = User.objects.all()
+
+	#then, prepare a list of all excursions
+	excursion_list = Excursion.objects.all()
+
+	#messages list
+	messages = []
+
+	offers=[]
+
+	#populate messages travelers put on their excursions
+	for excurj in excursion_list:
+		msg1 = "Hey there! nice to meet you. I see you're coming to %s, would love to meet up and show you around my hometown." 
+		msg2 = "Hey! my name is Sammy and would like to show you around %s and maybe meet up with my friends at the club, let me know if that works!" 
+		msg3 = "Ciao, Ciao! maybe I can go out with you tonight and introduce you to the best clubs around %s!"
+
+		messages.extend((msg3,msg2, msg1))
+
+	#generate 10 offers
+	for i in range(10):
+		trip=random.choice(excursion_list)
+		message = random.choice(messages) % (trip.city.name)
+		offer = Offer(local=random.choice(user_list), message=message, trip=trip,
+			traveler_approval = random.choice([True, False]))
+		offer.save()
+		offers.append(offer)
+
+	return offers 
+
+def populate_requests():
+	""" populate requests for when a traveler asks local to take him out upon liking his profile """
+	user_list = User.objects.all()
+	messages=[]
+	requests=[]
+	excursion_list = Excursion.objects.all()
+
+	#populate messages travelers put on their excursions
+	for excurj in excursion_list:
+		msg1 = "Hi! I'm coming to %s this month and I like your profile! would you want to take me out on a Saturday?" % excurj.city.name
+		msg2 = "Hi! I'm coming to %s over Christams, I'm available on Friday, do you wanna meet up?" % excurj.city.name
+		msg3 = "Ciao, Ciao! can I go out with you this weekend? leaving %s on Monday :( :D" % excurj.city.name
+
+		messages.extend((msg3,msg2, msg1))
+
+	#generate 10 requests
+	for i in range(10):
+		request = Request(traveler=random.choice(user_list), local=random.choice(user_list), 
+			message=random.choice(messages), date=generate_date(), local_approval=random.choice([True, False]))
+
+		request.save()
+		requests.append(request)
+
+	return requests
+
 if __name__ == '__main__':
 	print('starting populate.py')
 	# populate_cities()
-	populate_users()
+	# populate_users()
+	# populate_excursions()
+	# populate_offers()
+	populate_requests()
 
